36554d107dfd5a9c131ad39e78711daf0b6992e3
sarenord <ajsheld12@gmail.com>
2019-12-03 16:12:29 -0500

    added pdf-tools configuration to init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -17,19 +17,73 @@
 
 
 ;;; packages to install/use
-(use-package ispell)
-(use-package helm)
-(use-package ewal
+(use-package ispell :ensure t)
+(use-package helm :ensure t)
+
+(use-package org-bullets :ensure t
+  :config (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+  )
+
+
+(use-package ewal :ensure t
 	     :init (setq ewal-use-built-in-always-p nil
 			 ewal-use-built-in-on-failure-p t
 			 ewal-built-in-palette "sexy material"
 			 ewal-json-file "~/.cache/wal/colors.json")
-	     :ensure t)
+	     )
 (use-package ewal-spacemacs-themes
 	:config (progn
 		  (load-theme 'ewal-spacemacs-modern t)
-		  (enable-theme 'ewal-spacemacs-modern)))
-(use-package pdf-tools)
+		  (enable-theme 'ewal-spacemacs-modern))
+	)
+
+(use-package ivy :ensure t
+  :diminish (ivy-mode . "")
+  :init (ivy-mode 1)
+  :bind (:map ivy-mode-map
+	      ("C-'" . ivy-avy))
+  :config
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-height 20)
+  (setq ivy-count-format "(%d/%d) ")
+  )
+(use-package counsel :ensure t
+  :bind*
+  (("M-x" . counsel-M-x)
+   ("C-x C-f" . counsel-find-file)
+   ("C-x C-r" . counsel-recentf)
+   ("C-c f" . counsel-git)
+   ("C-c s" . counsel-git-grep)
+   ("C-c /" . counsel-ag)
+   ("C-c l" . counsel-locate))
+   )
+
+(use-package which-key :ensure t
+  :init (which-key-mode)
+  :config
+  (which-key-setup-side-window-right-bottom)
+  (setq which-key-sort-order 'which-key-key-order-alpha
+        which-key-side-window-max-width 0.33
+        which-key-idle-delay 0.05)
+  )
+
+(use-package pdf-tools
+  :config
+  (pdf-tools-install)
+  (setq-default pdf-view-display-size 'fit-page)
+  (define-key pdf-view-mode-map (kbd "C-s") 'isearch-forward)
+  (add-hook 'pdf-view-mode-hook (lambda () (cua-mode 0)))
+
+  (define-key pdf-view-mode-map (kbd "h") 'image-backward-hscroll)
+  (define-key pdf-view-mode-map (kbd "j") 'pdf-view-next-line-or-next-page)
+  (define-key pdf-view-mode-map (kbd "k") 'pdf-view-previous-line-or-next-page)
+  (define-key pdf-view-mode-map (kbd "l") 'image-forward-hscroll)
+  )
+(use-package key-chord
+  :init (key-chord-mode 1)
+  :config
+  )
+
 
 
 ;;; some configuration commands
@@ -37,9 +91,8 @@
 (tool-bar-mode -1)
 (toggle-scroll-bar -1)
 
-;;; variable setting
-
-
+;;; key bindings
+(key-chord-define-global "  " 'helm-M-x)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -70,7 +123,7 @@
      ("\\?\\?\\?+" . "#428568"))))
  '(package-selected-packages
    (quote
-    (pdf-tools ewal-spacemacs-themes ewal use-package org-evil evil-visual-mark-mode)))
+    (org-bullets which-key ivy go-mode pdf-tools ewal-spacemacs-themes ewal use-package org-evil evil-visual-mark-mode)))
  '(pdf-view-midnight-colors (quote ("#bfcbb3" . "#021110"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
