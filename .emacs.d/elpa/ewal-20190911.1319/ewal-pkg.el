2ca95a1d5ff41f4c78cb57b95a38a89be453f0cc
sarenord <ajsheld12@gmail.com>
2019-12-02 20:20:14 -0500

    test commit

---
+++ .emacs.d/elpa/ewal-20190911.1319/ewal-pkg.el
@@ -0,0 +1,12 @@
+(define-package "ewal" "20190911.1319" "A pywal-based theme generator"
+  '((emacs "25"))
+  :keywords
+  '("faces")
+  :authors
+  '(("Uros Perisic"))
+  :maintainer
+  '("Uros Perisic")
+  :url "https://gitlab.com/jjzmajic/ewal")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
