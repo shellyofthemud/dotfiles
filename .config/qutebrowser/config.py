e4a9e04ea3060269ed076ee3f76730cb996444d4
sarenord <ajsheld12@gmail.com>
2019-12-03 21:45:34 -0500

    added search engines and lastpass to qutebrowser

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -2,8 +2,17 @@
 config.load_autoconfig()
 c.backend = 'webengine'
 
-''' load stylesheet solarized dark '''
+# adding some search engines
+c.url.searchengines = {
+    "DEFAULT": "https://duckduckgo.com/?q={}",
+    "g": "https://google.com/search?q={}",
+    "aw": "https://wiki.archlinux.org/index.php?search={}"
+    }
+
+# setting some extra keys to functions
 config.bind(',d', 'config-cycle --temp content.user_stylesheets ./solarized-dark-all-sites.css ""')
+config.bind(',f', 'spawn --userscript qute-lastpass')
+
 
 ''' load pywal configuration qutewal.py '''
 config.source('qutewal.py')
