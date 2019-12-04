484ab93df9c2e885ab48e965f04c264e4ebcc141
sarenord <ajsheld12@gmail.com>
2019-12-03 22:16:04 -0500

    attempting to add portable network device configuration

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export WLAN=$(ip a | grep -m 1 wlp | cut -f 2 -d " " | sed 's/://')
+export ETH=$(ip a | grep -m 1 enp | cut -f 2 -d " " | sed 's/://')
+
 killall -q polybar
 while pgrep -x polybar > /dev/null; do sleep 1; done
 
