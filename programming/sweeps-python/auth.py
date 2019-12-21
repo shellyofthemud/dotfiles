18af9a44765c59b718895f985b9c22ae4031385b
sarenord <ajsheld12@gmail.com>
2019-12-20 20:25:45 -0500

    moved feh execution to i3config

---
+++ programming/sweeps-python/auth.py
@@ -0,0 +1,21 @@
+import configparser
+import json
+import requests
+    
+class Sweeps():
+    @staticmethod
+    def getboi():
+        
+
+
+class Request():
+    def __init__(boi_id, command, direction):
+        this.id = boi_id
+        this.command = command
+        this.direction = direction
+
+    
+    def send():
+        if this.endpoint == "commandBoi":
+            r = requests.put(URL+'commandBoi', )
+            
---
+++ programming/sweeps-python/entity.py
@@ -0,0 +1,12 @@
+class Entity:
+    def __init__(id):
+
+    @staticmethod
+    def getmyentities:
+
+    def move(direction):
+        r = new Request()
+        
+    def detect(direction):
+
+    def eat(direction):
