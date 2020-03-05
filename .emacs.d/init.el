321a76c9d03ad7a67b4e4525e989c5984d9d9211
sarenord <ajsheld12@gmail.com>
2020-03-04 19:08:29 -0500

    fixed some pywal stuff

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -33,7 +33,8 @@
 (use-package ewal-spacemacs-themes
   :config (progn
 	    (load-theme 'ewal-spacemacs-modern t)
-	    (enable-theme 'ewal-spacemacs-modern))
+	    (enable-theme 'ewal-spacemacs-modern)
+	    )
   )
 
 (use-package ivy :ensure t
@@ -46,6 +47,7 @@
   (setq ivy-height 20)
   (setq ivy-count-format "(%d/%d) ")
   )
+
 (use-package counsel :ensure t
   :bind*
   (("M-x" . counsel-M-x)
@@ -85,62 +87,60 @@
   :config
   )
 
+;;; Major mode packages
 (use-package python-mode :ensure t
-  :config
-  (add-hook 'python-mode-hook
+  :config  (add-hook 'python-mode-hook
 	    (lambda ()
-	      (setq-default indent-tabs-mode t)
+	      (setq-default indent-tabs-mode f)
 	      (setq-default tab-width 4)
-	      (setq-default py-indent-tabs-mode t))
-	    (add-to-list 'write-file-functions 'delete-trailing-whitespace)
-	    ))
-
-
+	      (setq-default py-indent-tabs-mode f))
+	    (add-to-list 'write-file-functions 'delete-trailing-whitespace)))
 
 ;;; some configuration commands
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(toggle-scroll-bar -1)
-(setq make-backup-files nil)
+		(menu-bar-mode -1)
+		(tool-bar-mode -1)
+		(toggle-scroll-bar -1)
+		(setq make-backup-files nil)
 
 ;;; key bindings
-(key-chord-define-global "  " 'counsel-M-x)
-(key-chord-define-global ",c" 'comment-region)
+		(key-chord-define-global "  " 'counsel-M-x)
+		(key-chord-define-global ",c" 'comment-region)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#010d0c" "#2D6354" "#4E7151" "#428568" "#619369" "#B1A244" "#90A96C" "#bfcbb3"])
- '(custom-safe-themes
-   (quote
-    ("41098e2f8fa67dc51bbe89cce4fb7109f53a164e3a92356964c72f76d068587e" default)))
- '(hl-todo-keyword-faces
-   (quote
-    (("TODO" . "#428568")
-     ("NEXT" . "#428568")
-     ("THEM" . "#90A96C")
-     ("PROG" . "#619369")
-     ("OKAY" . "#619369")
-     ("DONT" . "#2D6354")
-     ("FAIL" . "#2D6354")
-     ("DONE" . "#718d73")
-     ("NOTE" . "#428568")
-     ("KLUDGE" . "#428568")
-     ("HACK" . "#428568")
-     ("TEMP" . "#428568")
-     ("FIXME" . "#428568")
-     ("XXX+" . "#428568")
-     ("\\?\\?\\?+" . "#428568"))))
- '(package-selected-packages
-   (quote
-    (python-mode web-mode latex-preview-pane org-bullets which-key ivy go-mode pdf-tools ewal-spacemacs-themes ewal use-package org-evil evil-visual-mark-mode)))
- '(pdf-view-midnight-colors (quote ("#bfcbb3" . "#021110"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;;; emacs setting storage
+		(custom-set-variables
+		 ;; custom-set-variables was added by Custom.
+		 ;; If you edit it by hand, you could mess it up, so be careful.
+		 ;; Your init file should contain only one such instance.
+		 ;; If there is more than one, they won't work right.
+		 '(ansi-color-names-vector
+		   ["#010d0c" "#2D6354" "#4E7151" "#428568" "#619369" "#B1A244" "#90A96C" "#bfcbb3"])
+		 '(custom-safe-themes
+		   (quote
+		    ("41098e2f8fa67dc51bbe89cce4fb7109f53a164e3a92356964c72f76d068587e" default)))
+		 '(hl-todo-keyword-faces
+		   (quote
+		    (("TODO" . "#428568")
+		     ("NEXT" . "#428568")
+		     ("THEM" . "#90A96C")
+		     ("PROG" . "#619369")
+		     ("OKAY" . "#619369")
+		     ("DONT" . "#2D6354")
+		     ("FAIL" . "#2D6354")
+		     ("DONE" . "#718d73")
+		     ("NOTE" . "#428568")
+		     ("KLUDGE" . "#428568")
+		     ("HACK" . "#428568")
+		     ("TEMP" . "#428568")
+		     ("FIXME" . "#428568")
+		     ("XXX+" . "#428568")
+		     ("\\?\\?\\?+" . "#428568"))))
+		 '(package-selected-packages
+		   (quote
+		    (jdee python-mode web-mode latex-preview-pane org-bullets which-key ivy go-mode pdf-tools ewal-spacemacs-themes ewal use-package org-evil evil-visual-mark-mode)))
+		 '(pdf-view-midnight-colors (quote ("#bfcbb3" . "#021110"))))
+		(custom-set-faces
+		 ;; custom-set-faces was added by Custom.
+		 ;; If you edit it by hand, you could mess it up, so be careful.
+		 ;; Your init file should contain only one such instance.
+		 ;; If there is more than one, they won't work right.
+		 )
