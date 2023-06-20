3e3eed79c4f231a399dc4d54149f4ac2e19fe53f
sheldrick <ajsheld12@gmail.com>
2023-06-20 00:07:25 -0400

    branched to multiple bars, redoing colors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,41 +5,58 @@ export MON2=$(xrandr -q | grep ' connected ' | grep -v primary | cut -f 1 -d" ")
 
 WINDOW_ID_BASE=/tmp/polybar_base_window_id
 WINDOW_ID_TOP=/tmp/polybar_top_window_id
+WINDOW_ID_TOP_TWO=/tmp/polybar_top_window_id_2
+WINDOW_ID_TRAY=/tmp/polybar_tray_window_id
 
 
 polybar_launch() {
+    killall -q picom
     killall -q polybar
     while pgrep -x polybar > /dev/null; do sleep 1; done
 
     polybar main &
     xdotool search --sync --pid $! | cut -f 1 -d$'\n' > $WINDOW_ID_BASE
 
-	[[ $(command -v blueman-applet) ]] && blueman-applet
+    polybar launcher &
+    xdotool search --sync --pid $! | cut -f 1 -d$'\n' > $WINDOW_ID_TOP
 
+    polybar secondary &
+    xdotool search --sync --pid $! | cut -f 1 -d$'\n' > $WINDOW_ID_TOP_TWO
 
-    # polybar app_menu &
-    # xdotool search --sync --pid $! > $WINDOW_ID_TOP
+    polybar tray &
+    xdotool search --sync --pid $! | cut -f 1 -d$'\n' > $WINDOW_ID_TRAY
 
-    # bar_collapse
+    bar_collapse
 }
 
 rofi_open() {
     bar_expand &
-    rofi -modi run -show run
+    rofi -modi run -show run -location 1 -theme menu
     bar_collapse
+}
+
+bar_expand() {
+
+    # xdotool windowunmap $(cat $WINDOW_ID_BASE)
+    xdotool windowmap --sync $(cat $WINDOW_ID_TOP)
+    for y in `seq 430 10 570`; do
+	xdotool windowmove --sync  $(cat $WINDOW_ID_TOP) x $y
+    done
 }
 
 bar_collapse() {
     xdotool windowunmap $(cat $WINDOW_ID_TOP)
     xdotool windowmap $(cat $WINDOW_ID_BASE)
-}
-
-bar_expand() {
-    # xdotool windowunmap $(cat $WINDOW_ID_BASE)
-    xdotool windowmap $(cat $WINDOW_ID_TOP)
+    xdotool windowmove --sync $(cat $WINDOW_ID_TOP) x 430
 }
 
 case "$1" in
+    test)
+	bar_expand
+	sleep 1
+	bar_collapse
+	;;
+
     app_menu)
 	rofi_open
 	;;
@@ -49,5 +66,6 @@ case "$1" in
 	;;
 
     *)
-	polybar_launch &;;
+	polybar_launch &
+	;;
 esac
